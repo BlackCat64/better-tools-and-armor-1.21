994a5687c85c2d90b6315042210c0c9ac0fc823f
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-08-03 11:24:51 +0100

    Fixed Ender Goggles

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/EnderGogglesProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/EnderGogglesProcedureProcedure.java
@@ -1,6 +1,6 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
-import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingChangeTargetEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.ICancellableEvent;
@@ -9,7 +9,6 @@ import net.neoforged.bus.api.Event;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.monster.EnderMan;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
@@ -19,12 +18,10 @@ import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 import javax.annotation.Nullable;
 
 @EventBusSubscriber
-public class EnderTitaniumHelmetProcedureProcedure {
+public class EnderGogglesProcedureProcedure {
 	@SubscribeEvent
-	public static void onEntityAttacked(LivingIncomingDamageEvent event) {
-		if (event.getEntity() != null) {
-			execute(event, event.getEntity().level(), event.getEntity(), event.getSource().getEntity());
-		}
+	public static void onEntitySetsAttackTarget(LivingChangeTargetEvent event) {
+		execute(event, event.getEntity().level(), event.getOriginalAboutToBeSetTarget(), event.getEntity());
 	}
 
 	public static void execute(LevelAccessor world, Entity entity, Entity sourceentity) {
@@ -34,14 +31,10 @@ public class EnderTitaniumHelmetProcedureProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
-		if (sourceentity instanceof EnderMan) {
-			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY).getItem() == BetterToolsModItems.END_TITANIUM_HELMET.get()) {
-				if (event instanceof ICancellableEvent _cancellable) {
-					_cancellable.setCanceled(true);
-				}
-			} else if (HasCuriosItemEquippedProcedure.execute(world, entity, new ItemStack(BetterToolsModItems.ENDER_GOGGLES.get()))) {
-				if (sourceentity instanceof Mob _entity)
-					_entity.setTarget(null);
+		if (((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY).getItem() == BetterToolsModItems.END_TITANIUM_HELMET.get()
+				|| HasCuriosItemEquippedProcedure.execute(world, entity, new ItemStack(BetterToolsModItems.ENDER_GOGGLES.get()))) && sourceentity instanceof EnderMan) {
+			if (event instanceof ICancellableEvent _cancellable) {
+				_cancellable.setCanceled(true);
 			}
 		}
 	}
