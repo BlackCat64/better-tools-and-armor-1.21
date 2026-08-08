69105a35c39e4189a308193a66c7351278510c2e
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-08-08 09:14:14 +0100

    Crystallite Netherite Pickaxe refactor

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteNetheriteDeepslateProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteNetheriteDeepslateProcedureProcedure.java
@@ -1,14 +1,13 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
@@ -16,56 +15,34 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.core.BlockPos;
-import net.minecraft.client.multiplayer.PlayerInfo;
-import net.minecraft.client.Minecraft;
 
 import javax.annotation.Nullable;
 
 @EventBusSubscriber
 public class CrystalliteNetheriteDeepslateProcedureProcedure {
 	@SubscribeEvent
-	public static void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event) {
-		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getLevel().getBlockState(event.getPos()), event.getEntity());
+	public static void onBlockBreaking(PlayerEvent.BreakSpeed event) {
+		if (event.getPosition().isEmpty())
+			return;
+		execute(event, event.getEntity().level(), event.getState(), event.getEntity(), event.getNewSpeed());
 	}
 
-	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate, Entity entity) {
-		execute(null, world, x, y, z, blockstate, entity);
+	public static void execute(LevelAccessor world, BlockState blockstate, Entity entity, double breakSpeed) {
+		execute(null, world, blockstate, entity, breakSpeed);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, BlockState blockstate, Entity entity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, BlockState blockstate, Entity entity, double breakSpeed) {
 		if (entity == null)
 			return;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:deep_mining_tools"))) && !(getEntityGameType(entity) == GameType.CREATIVE)) {
-			if (blockstate.is(BlockTags.create(ResourceLocation.parse("forge:stone")))
-					&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.EFFICIENCY)) != 0) {
-				if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.EFFICIENCY)) >= 5) {
-					{
-						BlockPos _pos = BlockPos.containing(x, y, z);
-						Block.dropResources(world.getBlockState(_pos), world, BlockPos.containing(x, y, z), null);
-						world.destroyBlock(_pos, false);
-					}
-					if (world instanceof ServerLevel _level) {
-						(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).hurtAndBreak(1, _level, null, _stkprov -> {
-						});
-					}
-				}
-			}
+		if ((blockstate.is(BlockTags.create(ResourceLocation.parse("c:stones"))) || blockstate.is(BlockTags.create(ResourceLocation.parse("better_tools:magma"))) || blockstate.getBlock() == Blocks.BASALT || blockstate.getBlock() == Blocks.BLACKSTONE)
+				&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:deep_mining_tools")))
+				&& !(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)
+				&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.EFFICIENCY)) != 0
+				&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.EFFICIENCY)) >= 5) {
+			if (event instanceof PlayerEvent.BreakSpeed _speed15)
+				_speed15.setNewSpeed((float) (breakSpeed * 1000));
 		}
-	}
-
-	private static GameType getEntityGameType(Entity entity) {
-		if (entity instanceof ServerPlayer serverPlayer) {
-			return serverPlayer.gameMode.getGameModeForPlayer();
-		} else if (entity instanceof Player player && player.level().isClientSide()) {
-			PlayerInfo playerInfo = Minecraft.getInstance().getConnection().getPlayerInfo(player.getGameProfile().getId());
-			if (playerInfo != null)
-				return playerInfo.getGameMode();
-		}
-		return null;
 	}
 }
