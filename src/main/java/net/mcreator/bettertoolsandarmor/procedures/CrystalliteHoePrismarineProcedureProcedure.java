568ecf316ba4eccd41de4585c2d6e80f8f454871
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-08-09 18:12:31 +0100

    Crystallite Hoe Prismarine refactoring

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoePrismarineProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoePrismarineProcedureProcedure.java
@@ -4,6 +4,7 @@ import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.ModList;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -16,14 +17,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
-
-import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
-import net.mcreator.bettertoolsandarmor.BetterToolsMod;
 
 import javax.annotation.Nullable;
 
@@ -44,44 +44,45 @@ public class CrystalliteHoePrismarineProcedureProcedure {
 		if (entity == null)
 			return;
 		BlockState current = Blocks.AIR.defaultBlockState();
-		boolean aquaculture = false;
 		double sx = 0;
 		double sz = 0;
 		double tilled = 0;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:hydration_hoes")))
+				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:hydration_hoes")))
 						&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == ItemStack.EMPTY.getItem()) {
-			if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.FARMLAND || (BuiltInRegistries.BLOCK.getKey((world.getBlockState(BlockPos.containing(x, y, z))).getBlock()).toString()).equals("aquaculture:farmland")
-					|| (world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.DIRT || (world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.GRASS_BLOCK) {
-				aquaculture = ModList.get().isLoaded("aquaculture");
+			if ((world.getBlockState(BlockPos.containing(x, y, z))).is(BlockTags.create(ResourceLocation.parse("better_tools:hoe_allowed_blocks")))
+					|| (world.getBlockState(BlockPos.containing(x, y, z))).is(BlockTags.create(ResourceLocation.parse("better_tools:farmland")))) {
+				if (event instanceof ICancellableEvent _cancellable) {
+					_cancellable.setCanceled(true);
+				}
 				sx = -1;
 				sz = -1;
 				tilled = 0;
-				for (int index46 = 0; index46 < 3; index46++) {
-					for (int index47 = 0; index47 < 3; index47++) {
-						if (!(sx == 0 && sz == 0)) {
-							current = (world.getBlockState(BlockPos.containing(x + sx, y, z + sz)));
-							if (!world.getBlockState(BlockPos.containing(x + sx, y + 1, z + sz)).canOcclude()) {
-								if (current.getBlock() == Blocks.FARMLAND && aquaculture || current.getBlock() == Blocks.DIRT || current.getBlock() == Blocks.GRASS_BLOCK) {
-									if (aquaculture) {
-										world.setBlock(BlockPos.containing(x + sx, y, z + sz), BuiltInRegistries.BLOCK.get(ResourceLocation.parse("aquaculture:farmland")).defaultBlockState(), 3);
-									} else {
-										world.setBlock(BlockPos.containing(x + sx, y, z + sz), Blocks.FARMLAND.defaultBlockState(), 3);
-										{
-											int _value = 7;
-											BlockPos _pos = BlockPos.containing(x + sx, y, z + sz);
-											BlockState _bs = world.getBlockState(_pos);
-											if (_bs.getBlock().getStateDefinition().getProperty("moisture") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-												world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-										}
+				for (int index2084 = 0; index2084 < 3; index2084++) {
+					for (int index2085 = 0; index2085 < 3; index2085++) {
+						current = (world.getBlockState(BlockPos.containing(x + sx, y, z + sz)));
+						if (!world.getBlockState(BlockPos.containing(x + sx, y + 1, z + sz)).canOcclude()) {
+							if (current.getBlock() == Blocks.FARMLAND && ModList.get().isLoaded("aquaculture") || current.is(BlockTags.create(ResourceLocation.parse("better_tools:hoe_allowed_blocks")))) {
+								if (ModList.get().isLoaded("aquaculture")) {
+									world.setBlock(BlockPos.containing(x + sx, y, z + sz), BuiltInRegistries.BLOCK.get(ResourceLocation.parse("aquaculture:farmland")).defaultBlockState(), 3);
+								} else {
+									world.setBlock(BlockPos.containing(x + sx, y, z + sz), Blocks.FARMLAND.defaultBlockState(), 3);
+									{
+										int _value = 7;
+										BlockPos _pos = BlockPos.containing(x + sx, y, z + sz);
+										BlockState _bs = world.getBlockState(_pos);
+										if (_bs.getBlock().getStateDefinition().getProperty("moisture") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+											world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
 									}
-									tilled = tilled + 1;
-									if (world instanceof Level _level) {
-										if (!_level.isClientSide()) {
-											_level.playSound(null, BlockPos.containing(x + sx, y, z + sz), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.hoe.till")), SoundSource.NEUTRAL, (float) 0.5, 1);
-										} else {
-											_level.playLocalSound((x + sx), y, (z + sz), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.hoe.till")), SoundSource.NEUTRAL, (float) 0.5, 1, false);
-										}
+								}
+								if (world instanceof Level _level)
+									_level.updateNeighborsAt(BlockPos.containing(x + sx, y, z + sz), _level.getBlockState(BlockPos.containing(x + sx, y, z + sz)).getBlock());
+								tilled = tilled + 1;
+								if (world instanceof Level _level) {
+									if (!_level.isClientSide()) {
+										_level.playSound(null, BlockPos.containing(x + sx, y, z + sz), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.hoe.till")), SoundSource.NEUTRAL, (float) 0.5, 1);
+									} else {
+										_level.playLocalSound((x + sx), y, (z + sz), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.hoe.till")), SoundSource.NEUTRAL, (float) 0.5, 1, false);
 									}
 								}
 							}
@@ -91,21 +92,8 @@ public class CrystalliteHoePrismarineProcedureProcedure {
 					sz = sz + 1;
 					sx = -1;
 				}
-				BetterToolsMod.queueServerWork(2, () -> {
-					if (!(BuiltInRegistries.BLOCK.get(ResourceLocation.parse("aquaculture:farmland")) == Blocks.AIR)) {
-						world.setBlock(BlockPos.containing(x, y, z), BuiltInRegistries.BLOCK.get(ResourceLocation.parse("aquaculture:farmland")).defaultBlockState(), 3);
-					} else {
-						{
-							int _value = 7;
-							BlockPos _pos = BlockPos.containing(x, y, z);
-							BlockState _bs = world.getBlockState(_pos);
-							if (_bs.getBlock().getStateDefinition().getProperty("moisture") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-								world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-						}
-					}
-				});
 				if (tilled > 0) {
-					if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()) {
+					if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:hydration_hoes")))) {
 						if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
 							if (world instanceof ServerLevel _level) {
 								(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).hurtAndBreak((int) tilled, _level, null, _stkprov -> {
