b62cf8b506741ba8ba7017f9b719a10b63f95999
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-08-10 10:47:51 +0100

    Fixed Crystallite Sword Honey combo mechanic

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/ComboTimeoutResetProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/ComboTimeoutResetProcedure.java
@@ -5,7 +5,10 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
 
 import net.mcreator.bettertoolsandarmor.network.BetterToolsModVariables;
 
@@ -25,6 +28,17 @@ public class ComboTimeoutResetProcedure {
 	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
+		if (entity.getData(BetterToolsModVariables.PLAYER_VARIABLES).time_since_last_attacked > 40) {
+			if (entity instanceof LivingEntity _entity) {
+				_entity.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_1"));
+			}
+			if (entity instanceof LivingEntity _entity) {
+				_entity.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_2"));
+			}
+			if (entity instanceof LivingEntity _entity) {
+				_entity.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_3"));
+			}
+		}
 		if (entity.getData(BetterToolsModVariables.PLAYER_VARIABLES).time_since_last_mined > 100
 				|| entity.getData(BetterToolsModVariables.PLAYER_VARIABLES).time_since_last_mined > 40 && entity.getData(BetterToolsModVariables.PLAYER_VARIABLES).block_mining_combo >= 9) {
 			{
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordHoneyProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordHoneyProcedureProcedure.java
@@ -6,6 +6,9 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.ItemTags;
@@ -31,18 +34,49 @@ public class CrystalliteSwordHoneyProcedureProcedure {
 	private static void execute(@Nullable Event event, Entity sourceentity) {
 		if (sourceentity == null)
 			return;
-		{
-			BetterToolsModVariables.PlayerVariables _vars = sourceentity.getData(BetterToolsModVariables.PLAYER_VARIABLES);
-			_vars.time_since_last_attacked = 0;
-			_vars.markSyncDirty();
-		}
-		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:combo_weapons")))) {
-			if (sourceentity.getData(BetterToolsModVariables.PLAYER_VARIABLES).time_since_last_attacked <= 40) {
-				if (false) {// Remove modifier 1, add modifier 2
-				} else if (false) {// Remove modifier 1+2, add modifier 3
-				}
+		if (sourceentity instanceof Player) {
+			{
+				BetterToolsModVariables.PlayerVariables _vars = sourceentity.getData(BetterToolsModVariables.PLAYER_VARIABLES);
+				_vars.time_since_last_attacked = 0;
+				_vars.markSyncDirty();
 			}
-			if (true) {// Add modifier 1
+			if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("better_tools:combo_weapons")))) {
+				if (sourceentity.getData(BetterToolsModVariables.PLAYER_VARIABLES).time_since_last_attacked <= 40) {
+					if (sourceentity instanceof LivingEntity _livingEntity3 && _livingEntity3.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_1"))) {
+						if (sourceentity instanceof LivingEntity _entity) {
+							_entity.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_1"));
+						}
+						if (sourceentity instanceof LivingEntity _entity) {
+							AttributeModifier modifier = new AttributeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_2"), 2, AttributeModifier.Operation.ADD_VALUE);
+							if (!_entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(modifier.id())) {
+								_entity.getAttribute(Attributes.ATTACK_DAMAGE).addPermanentModifier(modifier);
+							}
+						}
+					} else if (sourceentity instanceof LivingEntity _livingEntity6 && _livingEntity6.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_2"))) {
+						if (sourceentity instanceof LivingEntity _entity) {
+							_entity.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_1"));
+						}
+						if (sourceentity instanceof LivingEntity _entity) {
+							_entity.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_2"));
+						}
+						if (sourceentity instanceof LivingEntity _entity) {
+							AttributeModifier modifier = new AttributeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_3"), 3, AttributeModifier.Operation.ADD_VALUE);
+							if (!_entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(modifier.id())) {
+								_entity.getAttribute(Attributes.ATTACK_DAMAGE).addPermanentModifier(modifier);
+							}
+						}
+					}
+				}
+				if (!(sourceentity instanceof LivingEntity _livingEntity10 && _livingEntity10.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_1"))
+						|| sourceentity instanceof LivingEntity _livingEntity11 && _livingEntity11.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_2"))
+						|| sourceentity instanceof LivingEntity _livingEntity12 && _livingEntity12.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_3")))) {
+					if (sourceentity instanceof LivingEntity _entity) {
+						AttributeModifier modifier = new AttributeModifier(ResourceLocation.parse("better_tools:crystallite_sword_honey_1"), 1, AttributeModifier.Operation.ADD_VALUE);
+						if (!_entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(modifier.id())) {
+							_entity.getAttribute(Attributes.ATTACK_DAMAGE).addPermanentModifier(modifier);
+						}
+					}
+				}
 			}
 		}
 	}
