851c8a0d1ddfb223c6c2301fa46f4bc712225d4c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-08-16 13:41:04 +0100

    Crystallite Chestplate Honey - made procedures more tag-based

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateHoneyProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateHoneyProcedureProcedure.java
@@ -5,17 +5,14 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.resources.ResourceLocation;
 
 import net.mcreator.bettertoolsandarmor.network.BetterToolsModVariables;
-import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import javax.annotation.Nullable;
 
@@ -33,8 +30,8 @@ public class CrystalliteChestplateHoneyProcedureProcedure {
 	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
-		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_HONEY_CHESTPLATE.get()) {
-			if (!(entity instanceof LivingEntity _livingEntity2 && _livingEntity2.getAttribute(Attributes.MAX_HEALTH).hasModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey")))) {
+		if (IsWearingArmorTagProcedure.execute(entity, "better_tools:hive_heart_armor")) {
+			if (!(entity instanceof LivingEntity _livingEntity0 && _livingEntity0.getAttribute(Attributes.MAX_HEALTH).hasModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey")))) {
 				if (entity instanceof LivingEntity _entity) {
 					AttributeModifier modifier = new AttributeModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey"), Math.floor((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 1.5),
 							AttributeModifier.Operation.ADD_VALUE);
@@ -61,7 +58,7 @@ public class CrystalliteChestplateHoneyProcedureProcedure {
 					}
 				}
 			}
-		} else if (entity instanceof LivingEntity _livingEntity12 && _livingEntity12.getAttribute(Attributes.MAX_HEALTH).hasModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey"))) {
+		} else if (entity instanceof LivingEntity _livingEntity10 && _livingEntity10.getAttribute(Attributes.MAX_HEALTH).hasModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey"))) {
 			if (entity instanceof LivingEntity _entity) {
 				_entity.getAttribute(Attributes.MAX_HEALTH).removeModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey"));
 			}
@@ -71,11 +68,11 @@ public class CrystalliteChestplateHoneyProcedureProcedure {
 			if (entity instanceof LivingEntity _entity)
 				_entity.setHealth(
 						(float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1) + Math.min(entity instanceof Player _plr ? _plr.getAbsorptionAmount() : 0, entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1)));
-			if ((entity instanceof Player _plr ? _plr.getAbsorptionAmount() : 0) >= (entity instanceof LivingEntity _livingEntity20 && _livingEntity20.getAttributes().hasAttribute(Attributes.MAX_ABSORPTION)
-					? _livingEntity20.getAttribute(Attributes.MAX_ABSORPTION).getValue()
+			if ((entity instanceof Player _plr ? _plr.getAbsorptionAmount() : 0) >= (entity instanceof LivingEntity _livingEntity18 && _livingEntity18.getAttributes().hasAttribute(Attributes.MAX_ABSORPTION)
+					? _livingEntity18.getAttribute(Attributes.MAX_ABSORPTION).getValue()
 					: 0)) {
 				SetAbsorptionAmountProcedure.execute(entity,
-						entity instanceof LivingEntity _livingEntity21 && _livingEntity21.getAttributes().hasAttribute(Attributes.MAX_ABSORPTION) ? _livingEntity21.getAttribute(Attributes.MAX_ABSORPTION).getValue() : 0);
+						entity instanceof LivingEntity _livingEntity19 && _livingEntity19.getAttributes().hasAttribute(Attributes.MAX_ABSORPTION) ? _livingEntity19.getAttribute(Attributes.MAX_ABSORPTION).getValue() : 0);
 			}
 		}
 	}
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateHoneyTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateHoneyTooltipProcedure.java
@@ -11,10 +11,9 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
-
-import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import javax.annotation.Nullable;
 
@@ -35,7 +34,7 @@ public class CrystalliteChestplateHoneyTooltipProcedure {
 	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
 		if (entity == null || tooltip == null)
 			return;
-		if (itemstack.getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_HONEY_CHESTPLATE.get()) {
+		if (itemstack.is(ItemTags.create(ResourceLocation.parse("better_tools:hive_heart_armor")))) {
 			if (entity instanceof LivingEntity _livingEntity2 && _livingEntity2.getAttribute(Attributes.MAX_HEALTH).hasModifier(ResourceLocation.parse("better_tools:crystallite_chestplate_honey"))) {
 				tooltip.add(Component.literal(("\u00A79+" + (new java.text.DecimalFormat("##.##").format((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 3)) + " Max Absorption")));
 			} else {
