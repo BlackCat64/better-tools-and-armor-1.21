4e30e0736ed1aab670e1d8c29a2d113822fd322b
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-09-06 11:20:03 +0100

    Fixed Bark sometimes not dropping when stripping logs

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/StrippedWoodProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/StrippedWoodProcedureProcedure.java
@@ -10,45 +10,41 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Direction;
 
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import javax.annotation.Nullable;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 @EventBusSubscriber
 public class StrippedWoodProcedureProcedure {
 	@SubscribeEvent
 	public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-		if (event.getHand() != InteractionHand.MAIN_HAND)
-			return;
-		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getLevel().getBlockState(event.getPos()), event.getFace(), event.getEntity());
+		//		if (event.getHand() != InteractionHand.MAIN_HAND)
+		//			return;
+		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getLevel().getBlockState(event.getPos()), event.getFace(), event.getEntity(), event.getHand());
 	}
 
 	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate, Direction direction, Entity entity) {
-		execute(null, world, x, y, z, blockstate, direction, entity);
+		execute(null, world, x, y, z, blockstate, direction, entity, InteractionHand.MAIN_HAND);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, BlockState blockstate, Direction direction, Entity entity) {
-		if (direction == null || entity == null)
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, BlockState blockstate, Direction direction, Entity entity, InteractionHand hand) {
+		if (direction == null || entity == null || !(entity instanceof Player plr))
 			return;
-		ItemStack tool = ItemStack.EMPTY;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:axes")))) {
-			tool = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).copy();
-		} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:axes")))
-				&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == ItemStack.EMPTY.getItem()) {
-			tool = (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).copy();
-		}
-		if (!(tool.getItem() == ItemStack.EMPTY.getItem() || tool.is(ItemTags.create(ResourceLocation.parse("c:tools/multitool")))) && blockstate.is(BlockTags.create(ResourceLocation.parse("minecraft:logs")))
-				&& !(BuiltInRegistries.BLOCK.getKey(blockstate.getBlock()).toString()).contains(":stripped_") && !(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
+		ItemStack tool = plr.getItemInHand(hand);
+		if (!tool.is(ItemTags.AXES))
+			return;
+		if (!(tool.getItem() == ItemStack.EMPTY.getItem() || tool.is(ItemTags.create(ResourceLocation.parse("c:tools/multitool"))))
+				&& blockstate.is(BlockTags.create(ResourceLocation.parse("minecraft:logs"))) && !(BuiltInRegistries.BLOCK.getKey(blockstate.getBlock()).toString()).contains(":stripped_")
+				&& !plr.getAbilities().instabuild) { // Do not allow in Creative Mode
 			if (world instanceof ServerLevel _level) {
 				ItemEntity entityToSpawn = new ItemEntity(_level, (x + 0.5 + direction.getStepX()), (y + 0.5 + direction.getStepY()), (z + 0.5 + direction.getStepZ()), new ItemStack(BetterToolsModItems.BARK.get()));
 				entityToSpawn.setPickUpDelay(10);
